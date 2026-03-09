5e68b6e5c511a31c5d677c953363300840c83b2a
Jhonnathan Castro Galeano <castrokof@gmail.com>
2026-03-09 19:30:49 +0000

    Eliminar migraciones para agregar porcentaje_subsidio y rellenar clientes.estrato_id

--- resources/views/facturacion/facturas/index.blade.php
+++ resources/views/facturacion/facturas/index.blade.php
@@ -163,6 +163,15 @@
 </form>
 
 @endsection
+@section('scriptsPlugins')
+<link href="{{asset("assets/$theme/plugins/datatables-bs4/css/dataTables.bootstrap4.css")}}" rel="stylesheet" type="text/css"/>   
+<script src="{{asset("assets/$theme/plugins/datatables/jquery.dataTables.js")}}" type="text/javascript"></script>
+<script src="{{asset("assets/$theme/plugins/datatables-bs4/js/dataTables.bootstrap4.js")}}" type="text/javascript"></script>
+<script src="{{asset("assets/$theme/plugins/select2/js/select2.full.min.js")}}" type="text/javascript"></script>
+<!-- <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap4.min.css">
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script> -->
+@endsection
 
 @section('scripts')
 <script>
